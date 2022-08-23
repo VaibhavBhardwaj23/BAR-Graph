36dfd91baedf788a6c78d0b49d4003d032273d6d
VaibhavBhardwaj23 <99650074+VaibhavBhardwaj23@users.noreply.github.com>
2022-08-23 22:24:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+// MONDAY
+function dis1()
+{
+    document.querySelector(".monday").classList.remove("hide")
+}
+function hid1()
+{
+    document.querySelector(".monday").classList.add("hide")
+}
+function per1()
+{
+    document.querySelector(".M").classList.toggle("blue");
+        document.querySelector(".monday").classList.remove("hide")
+
+}
+// TUESDAY
+function dis2()
+{
+    document.querySelector(".tuesday").classList.remove("hide")
+}
+function hid2()
+{
+    document.querySelector(".tuesday").classList.add("hide")
+}
+function per2()
+{
+    document.querySelector(".T").classList.toggle("blue");
+        document.querySelector(".tuesday").classList.remove("hide")
+
+}
+// WEDNESDAY
+function dis3()
+{
+    document.querySelector(".wednesday").classList.remove("hide")
+}
+function hid3()
+{
+    document.querySelector(".wednesday").classList.add("hide")
+}
+function per3()
+{
+    document.querySelector(".W").classList.toggle("blue");
+        document.querySelector(".wednesday").classList.remove("hide")
+
+}
+// THURSDAY
+function dis4()
+{
+    document.querySelector(".thursday").classList.remove("hide")
+}
+function hid4()
+{
+    document.querySelector(".thursday").classList.add("hide")
+}
+function per4()
+{
+    document.querySelector(".TH").classList.toggle("blue");
+        document.querySelector(".thursday").classList.remove("hide")
+
+}
+// FRIDAY
+function dis5()
+{
+    document.querySelector(".friday").classList.remove("hide")
+}
+function hid5()
+{
+    document.querySelector(".friday").classList.add("hide")
+}
+function per5()
+{
+    document.querySelector(".FR").classList.toggle("blue");
+        document.querySelector(".friday").classList.remove("hide")
+
+}
+// SATURDAY
+function dis6()
+{
+    document.querySelector(".saturday").classList.remove("hide")
+}
+function hid6()
+{
+    document.querySelector(".saturday").classList.add("hide")
+}
+function per6()
+{
+    document.querySelector(".SAT").classList.toggle("blue");
+        document.querySelector(".saturday").classList.remove("hide")
+
+}
+// SUNDAY
+function dis7()
+{
+    document.querySelector(".sunday").classList.remove("hide")
+}
+function hid7()
+{
+    document.querySelector(".sunday").classList.add("hide")
+}
+function per7()
+{
+    document.querySelector(".SUN").classList.toggle("blue");
+        document.querySelector(".sunday").classList.remove("hide")
+
+}
